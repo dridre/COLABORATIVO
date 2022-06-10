d7022be2eeb4193aa30ddad1c703849fd90095ec
Elefantex <samuel_borque@hotmail.com>
2022-06-10 17:26:20 +0100

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let cartasLevantadas = 0
 
@@ -48,53 +47,5 @@ for (let index2 = division; index2 > 0; index2--) {
 
 
 
-=======
-
-let cartasLevantadas = 0
-
-function changeImage() {
-
-    if (document.getElementById("image").src = "/images/cuadrado.png") {
-        document.getElementById("image").src = "/images/rojo.png";
-    }
-    else if (document.getElementById("image").src = "/images/rojo.png") {
-        document.getElementById("image").src = "/images/cuadrado.png";
-    }
-    cartasLevantadas = cartasLevantadas + 1
-    console.log(cartasLevantadas)
-
-}
 
 
-let pareja = prompt("Introduce el numero de parejas")
-let parejas = parseInt(pareja) * 2
-//console.log(parejas)
-let division = parseInt(pareja)
-//console.log(division)
-
-
-
-let imagenes = document.querySelector("div.conjunto")
-let imagenes2 = document.querySelector("div.conjunto2")
-
-let rojo = "/images/cuadrado.png"
-
-for (let index = parejas; index > division; index--) {
-    imagenes.innerHTML += '<img src="/images/cuadrado.png" alt="" onclick="changeImage()" id="image">'
-
-
-
-}
-for (let index2 = division; index2 > 0; index2--) {
-    console.log(index2)
-
-    imagenes2.innerHTML += '<img src="/images/rojo.png" alt="" onclick="changeImage()" id="image" class="">'
-
-
-}
-
-
-
-
-
->>>>>>> 54e2070bb0c4d0a855248e8f90838ab94d35f17f
