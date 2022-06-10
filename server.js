89634e0086b48c9554bcfb4fe06d272861bb2bf1
dridre <>
2022-06-10 13:17:42 +0200

    Signed-off-by: dridre <>

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-app.use(express.static('public'));
